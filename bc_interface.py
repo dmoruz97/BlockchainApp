2cb0820c25fc1f285be0bd658396513580bcb12f
dmoruz97 <davide.moruzzi97@hotmail.it>
2020-03-07 18:18:32 +0100

    Addes new functionalities from IBM tutorial

---
+++ bc_interface.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+import requests
+import node_server.Blockchain
+import time
+import json
+
+# Initialize flask application
+from node_server import Blockchain
+
+app =  Flask(__name__)
+
+# Initialize a blockchain object.
+blockchain = Blockchain()
+
+
+# Send a new transaction
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ["author", "content"]
+
+    for field in required_fields:
+        if not tx_data.get(field):
+            return "Invalid transaction data", 404
+
+    tx_data["timestamp"] = time.time()
+
+    blockchain.add_new_transaction(tx_data)
+
+    return "Success", 201
+
+# Get a copy of the chain
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
+# Mine unconfirmed transactions
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+    if not result:
+        return "No transactions to mine"
+    return "Block #{} is mined.".format(result)
+
+@app.route('/pending_tx')
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
