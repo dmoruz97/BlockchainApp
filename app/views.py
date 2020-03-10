5ad9e016d48431fe5134b4d85bacff41bf3a2473
dmoruz97 <davide.moruzzi97@hotmail.it>
2020-03-10 22:20:48 +0100

    Started to develop point 4.1 of assignment

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 import requests
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, url_for
 
 from app import app
 
@@ -38,6 +38,17 @@ def fetch_blockchain():
         return chain["chain"], chain["length"]
 
 
+# Recap of Blockchain endpoint
+@app.route('/about', methods=['GET'])
+def index_old():
+    fetch_posts()
+    """return render_template('about_blockchain.html',
+                           title='Blockchain',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)"""
+
+
 # Main endpoint
 @app.route('/', methods=['GET'])
 def index():
@@ -49,15 +60,52 @@ def index():
                            node_address=CONNECTED_NODE_ADDRESS)
 
 
-# Main endpoint
-@app.route('/old', methods=['GET'])
-def index_old():
-    fetch_posts()
-    return render_template('index2.html',
-                           title='Blockchain index',
-                           posts=posts,
-                           node_address=CONNECTED_NODE_ADDRESS,
-                           readable_time=timestamp_to_string)
+# Add new record to the chain (point 4.1 of the assignment)
+@app.route('/add_record', methods=['GET', 'POST'])
+def add_record():
+    if request.method == 'GET':
+        if 'success' in request.args:
+            success = request.args.get('success')
+        else:
+            success = ""
+
+        return render_template('add_record.html',
+                               title='Add new record',
+                               success=success)
+    else:
+        if request.method == 'POST':
+            tran = {"TRANSACTION_ID": request.form.get('transaction_id'),
+                    "YEAR": request.form.get('year'),
+                    "DAY_OF_WEEK": request.form.get('day_of_week'),
+                    "FL_DATE": request.form.get('flight_date'),
+                    "OP_CARRIER_AIRLINE_ID": request.form.get('op_carrier_airline_id'),
+                    "OP_CARRIER_FL_NUM": request.form.get('op_carrier_fl_num'),
+                    "ORIGIN_AIRPORT_ID": request.form.get('originial_airport_id'),
+                    "ORIGIN": request.form.get('origin'),
+                    "ORIGIN_CITY_NAME": request.form.get('origin_city_name'),
+                    "ORIGIN_STATE_NM": request.form.get('origin_state_nm'),
+                    "DEST_AIRPORT_ID": request.form.get('dest_airport_id'),
+                    "DEST": request.form.get('dest'),
+                    "DEST_CITY_NAME": request.form.get('dest_city_name'),
+                    "DEST_STATE_NM": request.form.get('dest_state_nm'),
+                    "DEP_TIME": request.form.get('dep_time'),
+                    "DEP_DELAY": request.form.get('dep_delay'),
+                    "ARR_TIME": request.form.get('arr_time'),
+                    "ARR_DELAY": request.form.get('arr_delay'),
+                    "CANCELLED": request.form.get('cancelled'),
+                    "AIR_TIME": request.form.get('ait_time')
+                    }
+
+            """data = json.dumps(tran)
+            headers = {'Content-type': 'application/json'}
+            response = requests.post('http://127.0.0.1:8000/new_transaction', headers=headers, data=data)
+            if response.status_code == 200:
+                params = {'success': 'Record successfuly added!'}
+                return redirect(url_for('add_record'))
+                requests.get('http://127.0.0.1:8000/add_record', params=params)"""
+
+            return json.dumps({"status": "success"}), 200
+
 
 # Route for query status (point 4.2 of the assignment)
 @app.route('/query_status', methods=['GET'])
@@ -82,6 +130,7 @@ def get_status_from_airline_and_date():
 
     # Return to the homepage
     return redirect('/')
+
 
 # Endpoint to create a new transaction via our application
 @app.route('/submit', methods=['POST'])
